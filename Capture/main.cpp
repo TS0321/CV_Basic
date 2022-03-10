e08355d347986d2bc78297eed9010308be57870c
takatoshi-sato <takatoshi6321@gmail.com>
2022-03-10 22:56:20 +0900

    calib用データを撮影

--- Capture/main.cpp
+++ Capture/main.cpp
@@ -1,5 +1,6 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <string>
 
 class Camera
 {
@@ -30,11 +31,24 @@ int main(void)
 {
 	Camera cam;
 	cam.init(1, 1280, 720);
+
+	std::string save_path = "../../data/calib/";
+	int img_num = 0;
+
 	while (1)
 	{
 		cv::Mat img = cam.capture();
 		cv::imshow("img", img);
-		cv::waitKey(1);
+		int key = cv::waitKey(1);
+		if (key == 'c') {
+			std::cout << "save image " << img_num << std::endl;
+			cv::imwrite(save_path + "img_" + std::to_string(img_num++)+".png", img);
+			cv::waitKey(1);
+		}
+		if (key == 'q')
+		{
+			break;
+		}
 	}
 
 	return 0;
